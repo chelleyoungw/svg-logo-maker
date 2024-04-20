a1f3cd86d22adc2b9014a7ae42404275e4f7ef07
Chelle Wood <chelleyoungw@gmail.com>
2024-04-20 18:59:13 -0500

    added psuedocode

---
+++ index.js
@@ -0,0 +1,16 @@
+// Inquirer (node package manager) import
+const inquirer = require('inquirer');
+
+// File system module (node package manager) import
+const fs = require('fs');
+
+// Importing classes from .lib/shapes directory
+
+
+// Function writes the SVG file using user answers from inquirer prompts
+
+
+// Function uses inquirer .prompt to prompt the user to answer questions in the CLI and save user input
+
+
+// Calling promptUser function so inquirer prompts fire off when app is run
