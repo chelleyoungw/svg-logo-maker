a3f3e9350e4350e5e20c9d33e4a13251d87609e4
Chelle Wood <chelleyoungw@gmail.com>
2024-04-20 22:19:32 -0500

    added error for text longer than 3 characters then return

--- index.js
+++ index.js
@@ -32,8 +32,23 @@ function promptUser() {
             message: 'What text would you like the logo to display? (enter up to three characters)',
             name: 'text',
         },
-        // 
-    ]);
+        // Text color prompt
+        {
+            type: 'input',
+            message: 'What color would you like the text to be? (enter color keyword OR a hexadecimal number)',
+            name: 'textColor',
+        },
+    ])
+    .then((answers) => {
+        // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+        if (answers.text.length > 3) {
+            console.log('Error: Text must be 3 characters or less');
+            return;
+        } else {
+            // Calling write file function to generate SVG file
+            writeToFile('logo.svg', answers);
+        }
+    });
 }
 
 // Calling promptUser function so inquirer prompts fire off when app is run
